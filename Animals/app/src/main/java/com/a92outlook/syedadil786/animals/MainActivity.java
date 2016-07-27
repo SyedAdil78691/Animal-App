818756b460a256b9de60ade4894bb0ec77ece61e
SyedAdil78691 <syedadil786.92@outlook.com>
2016-07-27 10:23:30 +0500

    ViewPager Added
    add Viewpager and Fargment add properly

--- Animals/app/src/main/java/com/a92outlook/syedadil786/animals/MainActivity.java
+++ Animals/app/src/main/java/com/a92outlook/syedadil786/animals/MainActivity.java
@@ -1,15 +1,27 @@
 package com.a92outlook.syedadil786.animals;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
-import android.view.MenuItem;
-
+import android.view.ViewGroup;
+import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
+
+    int [] mResources = {
+            R.drawable.darood1,
+            R.drawable.darood2,
+            R.drawable.darood3
+    };
+
+    MyFragmentAdapter mAdapter;
+    ViewPager mPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,29 @@ public class MainActivity extends AppCompatActivity {
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
+
+        mAdapter = new MyFragmentAdapter(getSupportFragmentManager());
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+//        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {
+//                Log.d("PositiononSelected", ""+position);
+//
+//
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//                Log.d("PositiononPage", ""+state);
+//
+//            }
+//        });
     }
 
     @Override
@@ -27,18 +62,50 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+    public class MyFragmentAdapter extends FragmentPagerAdapter {
+
+        public MyFragmentAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        // This method returns the fragment associated with the specified position.
+        //
+        // It is called when the Adapter needs a fragment and it does not exists.
+        public Fragment getItem(int position) {
+
+            Log.d("Position Fragment ", ""+position);
+            // Create fragment object
+            Fragment fragment = new DataFragment();
+            // Attach some data to it that we'll use to populate our fragment layouts
+            Bundle args = new Bundle();
+            args.putInt("page_position", position);
+
+            // Set the arguments on the fragment
+            // that will be fetched in DemoFragment@onCreateView
+            fragment.setArguments(args);
+            return fragment;
+        }
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+
+    public class DataFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            // Inflate the layout resource that'll be returned
+            View rootView = inflater.inflate(R.layout.fragmentdemo, container, false);
+
+            // Get the arguments that was supplied when the fragment was instantiated in the CustomPagerAdapter
+            Bundle args = getArguments();
+            int position = args.getInt("page_position");
+            ((ImageView) rootView.findViewById(R.id.image)).setImageResource(mResources[position]);
+            return rootView;
+        }
     }
 }
