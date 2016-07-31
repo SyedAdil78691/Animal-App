49bc7dba4a18dea122bce2d8ce6e398381f5b3b7
SyedAdil78691 <syedadil786.92@outlook.com>
2016-07-31 18:14:51 +0500

    not complete
    try to do

--- Animals/app/src/main/java/com/a92outlook/syedadil786/animals/MainActivity.java
+++ Animals/app/src/main/java/com/a92outlook/syedadil786/animals/MainActivity.java
@@ -1,5 +1,7 @@
 package com.a92outlook.syedadil786.animals;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -11,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +30,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT < 16) { //ye olde method
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else { // Jellybean and up, new hotness
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            // Remember that you should never show the action bar if the
+            // status bar is hidden, so hide that too if necessary.
+            android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
+        }
+
+
+
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
@@ -34,25 +56,26 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new MyFragmentAdapter(getSupportFragmentManager());
         mPager = (ViewPager)findViewById(R.id.pager);
         mPager.setAdapter(mAdapter);
-//        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) {
-//                Log.d("PositiononSelected", ""+position);
-//
-//
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//                Log.d("PositiononPage", ""+state);
-//
-//            }
-//        });
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d("PositiononSelected", ""+position);
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                Log.d("PositiononPage", ""+state);
+
+            }
+
+        });
     }
 
     @Override
